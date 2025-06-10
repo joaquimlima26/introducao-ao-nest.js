2eb88274cd61a30e3cf67fd0927e5528bbe6bb01
Joaquim Lima <joaquimzap9@gmail.com>
2025-06-10 14:57:52 -0300

    feat: adiciona prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.Module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   controllers: [],
   providers: [],
 })
