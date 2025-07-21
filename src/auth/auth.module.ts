6c6651453d2d8ac9c6f73892929e66dbba8a22ef
Joaquim Lima <joaquimzap9@gmail.com>
2025-07-21 16:14:18 -0300

    feat: update with Google

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.cotroller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { OAuth2Client } from 'google-auth-library';
+import { GoogleService } from './google-auth.service';
 
 @Module({
   imports: [
@@ -12,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
       signOptions: {expiresIn: '1d'}
     })
   ],
-  providers: [AuthService, PrismaService, JwtStrategy],
+  providers: [AuthService, PrismaService, JwtStrategy, GoogleService],
   controllers: [AuthController]
 })
 export class AuthModule {}
